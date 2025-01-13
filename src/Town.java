01e28a88c4d780a713cd1c77c764f027232aa73a
BT_1N3_15 <sample.bths@redu>
2025-01-13 13:42:09 -0500

    completed TH-1 (added colors class)

--- src/Town.java
+++ src/Town.java
@@ -99,22 +99,22 @@ public class Town {
         if (Math.random() > noTroubleChance) {
             printMessage = "You couldn't find any trouble";
         } else {
-            printMessage = Hunter.Colors.RED + "You want trouble, stranger!  You got it!\nOof! Umph! Ow!\n" + Hunter.Colors.RESET;
+            printMessage = Colors.RED + "You want trouble, stranger!  You got it!\nOof! Umph! Ow!\n" + Colors.RESET;
             int goldDiff = (int) (Math.random() * 10) + 1;
             if (Math.random() > noTroubleChance) {
-                printMessage += Hunter.Colors.RED +"Okay, stranger! You proved yer mettle. Here, take my gold." + Hunter.Colors.RESET;
-                printMessage += "\nYou won the brawl and receive " + Hunter.Colors.YELLOW + goldDiff + Hunter.Colors.RESET + " gold.";
+                printMessage += Colors.RED +"Okay, stranger! You proved yer mettle. Here, take my gold." + Colors.RESET;
+                printMessage += "\nYou won the brawl and receive " + Colors.YELLOW + goldDiff + Colors.RESET + " gold.";
                 hunter.changeGold(goldDiff);
             } else {
-                printMessage += Hunter.Colors.RED + "That'll teach you to go lookin' fer trouble in MY town! Now pay up!" + Hunter.Colors.RESET;
-                printMessage += "\nYou lost the brawl and pay " + Hunter.Colors.YELLOW + goldDiff + Hunter.Colors.RESET + " gold.";
+                printMessage += Colors.RED + "That'll teach you to go lookin' fer trouble in MY town! Now pay up!" + Colors.RESET;
+                printMessage += "\nYou lost the brawl and pay " + Colors.YELLOW + goldDiff + Colors.RESET + " gold.";
                 hunter.changeGold(-goldDiff);
             }
         }
     }
 
     public String infoString() {
-        return "This nice little town is surrounded by " + Hunter.Colors.CYAN + terrain.getTerrainName() + Hunter.Colors.RESET + ".";
+        return "This nice little town is surrounded by " + Colors.CYAN + terrain.getTerrainName() + Colors.RESET + ".";
     }
 
     /**
