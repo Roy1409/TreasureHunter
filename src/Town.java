c83bda5716c3590b1b5d44159ba295eb4cb9c000
BT_1N3_15 <sample.bths@redu>
2025-01-13 13:31:12 -0500

    completed TH-1

--- src/Town.java
+++ src/Town.java
@@ -99,22 +99,22 @@ public class Town {
         if (Math.random() > noTroubleChance) {
             printMessage = "You couldn't find any trouble";
         } else {
-            printMessage = "You want trouble, stranger!  You got it!\nOof! Umph! Ow!\n";
+            printMessage = Hunter.Colors.RED + "You want trouble, stranger!  You got it!\nOof! Umph! Ow!\n" + Hunter.Colors.RESET;
             int goldDiff = (int) (Math.random() * 10) + 1;
             if (Math.random() > noTroubleChance) {
-                printMessage += "Okay, stranger! You proved yer mettle. Here, take my gold.";
-                printMessage += "\nYou won the brawl and receive " + goldDiff + " gold.";
+                printMessage += Hunter.Colors.RED +"Okay, stranger! You proved yer mettle. Here, take my gold." + Hunter.Colors.RESET;
+                printMessage += "\nYou won the brawl and receive " + Hunter.Colors.YELLOW + goldDiff + Hunter.Colors.RESET + " gold.";
                 hunter.changeGold(goldDiff);
             } else {
-                printMessage += "That'll teach you to go lookin' fer trouble in MY town! Now pay up!";
-                printMessage += "\nYou lost the brawl and pay " + goldDiff + " gold.";
+                printMessage += Hunter.Colors.RED + "That'll teach you to go lookin' fer trouble in MY town! Now pay up!" + Hunter.Colors.RESET;
+                printMessage += "\nYou lost the brawl and pay " + Hunter.Colors.YELLOW + goldDiff + Hunter.Colors.RESET + " gold.";
                 hunter.changeGold(-goldDiff);
             }
         }
     }
 
     public String infoString() {
-        return "This nice little town is surrounded by " + terrain.getTerrainName() + ".";
+        return "This nice little town is surrounded by " + Hunter.Colors.CYAN + terrain.getTerrainName() + Hunter.Colors.RESET + ".";
     }
 
     /**
